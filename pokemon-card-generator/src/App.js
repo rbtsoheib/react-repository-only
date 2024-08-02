9ada1da8041929a092860c206e83d096b37d48a2
Nesrinels <lounissi.nesrine@gmail.com>
2024-08-02 13:13:19 +0100

    update

--- pokemon-card-generator/src/App.js
+++ pokemon-card-generator/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './Assets/R-removebg-preview.png';
 import './App.css';
 import Card from './Component/Card';
@@ -16,18 +17,18 @@ function App() {
           Search for your Pokemon
         </a>
       </header>
-      <body>
-      <div className="search">
-         <input type="text" placeholder='   search...' className=''/>
-         <button className='search-btn'>Search</button>
-      </div>
-       <div className='cards'>
-         <Card/>
-       </div>
-      <div className='load'>
-      <button className='load-btn'>Load more </button>
-      </div>
-      </body>
+      <main>
+        <div className="search">
+          <input type="text" placeholder='search...' className=''/>
+          <button className='search-btn'>Search</button>
+        </div>
+        <div className='cards'>
+          <Card />
+        </div>
+        <div className='load'>
+          <button className='load-btn'>Load more</button>
+        </div>
+      </main>
     </div>
   );
 }
--- pokemon-card-generator/src/Component/Card.js
+++ pokemon-card-generator/src/Component/Card.js
@@ -1,35 +1,52 @@
-
 import './Card.css';
+import { useEffect, useState } from "react";
 
 function Card() {
+  const [data, setData] = useState([]);
+  const [showData, setShowData] = useState(false);
+  const handleButtonClick = () => {
+    setShowData(!showData);
+  };
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((res) => res.json())
+      .then((data) =>
+        setData(
+          data.results.map((result) => ({
+            id: result.url.match(/\/([0-9]*)\/$/)[1],
+            name: result.name,
+            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+              result.url.match(/\/([0-9]*)\/$/)[1]
+            }.png`
+          }))
+        )
+      );
+  }, []);
+
   return (
-    <div className="Card">
-    
-
-        <div className='affichage'>
-
-            <img src='' alt=''></img>
-
-        </div>
-
-            <div className='writter'>
-
-            <h5>#number</h5>
-
-            <h6>exp: ...</h6>
-
-            <h2>Name</h2>
-
-            </div>
-            
-        <div className='btn'>
-
-            <button>
-                view more
+    <div>
+      {data.map((result) => (
+        <div className="Card" key={result.id}>
+          <div className='affichage'>
+            <img src={result.image} alt={result.name}></img>
+          </div>
+          <div className='writter'>
+            <h5>#{result.id}</h5>
+            <h2>{result.name}</h2>
+          </div>
+          <div className='btn'>
+            <button onClick={handleButtonClick}>
+              {showData ? "Hide" : "View more"}
             </button>
-
+            {showData && (
+              <div className='more-info'>
+                <h6>exp: ...</h6>
+              </div>
+            )}
+          </div>
         </div>
-
+      ))}
     </div>
   );
 }
